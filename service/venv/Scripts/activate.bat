f1074b3d08ec8d1de3e8cd3ad3cac88076d5842d
Bobby McGrath <56125566+bmcgrath89@users.noreply.github.com>
2020-02-13 23:49:08 +0000

    Update activate.bat

--- service/venv/Scripts/activate.bat
+++ service/venv/Scripts/activate.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "VIRTUAL_ENV=C:\Users\User\Desktop\React\service\venv"
+set "VIRTUAL_ENV=%systemdrive%%homepath%\service\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
